c495bc86025aef543015157478edeff76eaef04f
Chanchal Singh <44252494+Xblade007@users.noreply.github.com>
2021-11-01 15:06:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and implement
+          Learn React and implementing
         </a>
       </header>
     </div>
